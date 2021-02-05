781841626a05f75f765cf8a75e2ed55fe9783327
Barak Rosner <rosnerbb@gmail.com>
2021-02-05 03:09:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
     squared []
     count = 0 
-    while count < array.size do 
-    square_array [count] ** 2 
-    end 
+  while count < array.size do 
+    squared << (array[count] ** 2)
+    count + count + 1
+  end 
 end
